7a1d7a829a5d462c7ec9e0ca6ba4f8f1c6a19120
Jungincha <jungincha923@gmail.com>
2019-10-11 13:40:22 +0900

    Algorithm

--- Algorithm/solve.js
+++ Algorithm/solve.js
@@ -95,4 +95,115 @@ function findMissingNumber(str) {
     }
     return largest * (largest + 1) / 2 - sum;
   }
+
   
+/*
+어느날, 성준이는 우연히 길거리에서 양수 N을 보았다. 성준이는 30이란 수를 존경하기 때문에, 
+그는 길거리에서 찾은 수에 포함된 숫자들을 섞어 30의 배수가 되는 가장 큰 수를 만들고 싶어한다. 
+성준이를 도와 그가 만들고 싶어하는 수를 계산하는 프로그램을 작성하라. (그 수가 존재한다면)
+출력 : 성준이가 만들고 싶어하는 수가 존재한다면 그 수를 출력하라. 그 수가 존재하지 않는다면, -1을 출력하라.
+*/
+
+// My 
+
+// 30의 배수는 3의 배수이면서 일의 자리가 0인 수이다.
+// 3의 배수는 각 자리 숫자의 합이 3의 배수인 수이다.
+
+function findMaxOnMultiplesOfThirty(num) {
+  // 숫자들을 array에 넣기 + sort 내림차순으로 정렬
+  // 우선 모든 숫자들의 합이 3의 배수이면서 0이 존재하는지 검사 
+  // 검사 통과하면 숫자 합쳐서 리턴 아니면 -1반환
+
+  const strToInt = cur => parseInt(cur);
+  const bigToSmall = (a, b) => b - a;
+  const reduceFunc = (a, c) => a + c;
+
+  const numArr = String(num).split("").map(strToInt).sort(bigToSmall); 
+  
+  if(numArr[numArr.length - 1] === 0 && numArr.reduce(reduceFunc)) {
+    return Number(numArr.join(''));
+  } else {
+    return -1;
+  }
+}
+
+// Model
+
+function findMaxOnMultiplesOfThirty(num) {
+  var sum = 0;
+  var str = String(num)
+  var split = str.split('');
+  for(var i = 0; i < str.length; i++) {
+    sum = sum + Number(str[i]);
+  }
+  if(str.includes(0) === false || (sum % 3 !== 0)) {
+    return -1;
+  }
+  else if(str.includes(0) === true && (sum % 3 === 0)) {
+    split.sort(function(a,b) {
+      return b - a;
+    })
+    return Number(split.join(''));
+  }
+}
+
+/*
+Week 9-1 sumOnMultiplicationOfOrderedPairs
+입력한 숫자에 대해 서로 다른 두 숫자의 모든 경우에 대해, 곱의 합을 구하세요.
+sumOnMultiplicationOfOrderedPairs(2, 3, 4) // 26
+위 코드의 결과가 26이 나오는 이유는 다음을 참조하세요.
+
+가능한 조합(Pair)은 다음과 같습니다.
+
+(2, 3)
+(2, 4)
+(3, 4)
+*/
+
+// 삽질한 흔적... 이 코드는 args의 element가 중복이 되지 않을 때만 유효하다 
+
+function sumOnMultiplicationOfPairs(...args) {
+  // 모든 경우의 수의 조합을 만들어 array로 묶기(이 단계에서 더할 수 있으면 더하기)
+  // 그걸 큰 array에 집어넣기 (중복검사)
+  // 조합들을 모두 곱하고, 큰 array를 마저 더하기
+  let combArr = []; 
+  for(let i = 0; i < args.length; i++) {
+    for(let j = 0; j < args.length; j++) {
+     if(i !== j) {
+      let comb = [args[i], args[j]].sort((a,b) => a - b);
+      let newCombArr = combArr.map(cur => JSON.stringify(cur));
+      if(!(newCombArr.includes(JSON.stringify(comb)))) {
+        combArr.push(comb);
+      }
+     }
+    }
+  } 
+  const reduceMul = (a, c) => a * c;
+  const reduceAdd = (a, c) => a + c;
+  
+  return combArr.map(cur => cur.reduce(reduceMul)).reduce(reduceAdd);
+  
+}
+sumOnMultiplicationOfPairs(2, 3, 4); 
+
+// Model
+
+function sumOnMultiplicationOfPairs(...array) {
+  let sum = 0;
+  let result = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (i !== 0) {
+      result += sum * array[i];
+    }
+    sum += array[i];
+  }
+  return result;
+}
+
+
+
+
+
+
+
+
