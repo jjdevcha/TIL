7ed0f4403165984d5c37ba45eb06c101b56824cd
Jungincha <jungincha923@gmail.com>
2019-10-13 17:10:21 +0900

    20191013 Algorithms

--- Algorithm/solve.js
+++ Algorithm/solve.js
@@ -301,9 +301,131 @@ function runLength(str) {
   return rle;
 }
 
+/**
+ 문자열로 이루어진 배열이 주어졌을때, thirdGreatest(strArr) 함수는 주어진 배열에서 세번째로 긴 단어를 반환합니다. 
+ 그 중, 동률일때는 뒤에 있는 단어를 반환해줘야 합니다.
+예를 들어, 주어진 배열이 ["hello", "world", "before", "all"] 라면, 결과값은 world 가 될것입니다. 
+왜냐하면 before는 6글자이고 hello와 world 둘다 5글자 이지만 world가 더 뒤에 나온 5글자 단어이기 때문입니다.
+만약 주어진 배열이 ["hello", "world", "after", "all"] 이라면 결과값은 after가 될것입니다. 
+왜냐하면 앞에 세 단어가 모두 5글자이기 때문에 마지막 단어를 반환하기 때문입니다. 
+배열은 항상 적어도 문자열 세개를 가지고 있으며, 각각의 문자열은 오직 글자만을 포함하고 있습니다.
+ */
 
+// My 
 
+function thirdGreatest(arr) {
+  // sort로 배열 큰거부터 정리
+  // array에 세번째있는 애 추출
 
+  return arr.sort((a,b) => b.length - a.length)[2];
+}
+
+// Model
+
+function thirdGreatest(arr) {
+  arr = arr.sort(function(a, b) {
+    return b.length - a.length;
+  });
+  return arr[2];
+}
+
+/*
++, -, 그리고 괄호를 가지고 길이가 최대 50인 식을 만들었다. 
+그리고 나서 성준 이는 괄호를 모두 지웠다. 
+그리고 나서 괄호를 적절히 쳐서 이 식의 값을 최소로 만드려고 한다. 
+괄호를 적절히 쳐서 이 식의 값을 최소로 만드는 프로그램을 작성하시오.
+findMinByBracket('55-50+40') // -35
+*/
+
+// My 
+// pseudo 코드랑 좀 다르게 문제를 풀어나갔음
+// 1) '-'나오는 부분으로 split하기
+// 2) spit한 array를 돌며 eval로 계산해놓기
+// 3) join할 때 다시 '-'넣어주고 전체 str을 다시 '-'로 합쳐 eval로 return! 
+// 잼땨ㅑㅑㅑㅑㅑㅑ <3
+
+function findMinByBracket(str) {
+  
+  //str arr에다 넣고 
+  // iterator 사용해서 -나오면 () 집어넣기
+  // 다시  .join() 시키기
+  // eval 시켜서 return 하기 
+  let strArr = str.split('-');
+
+  strArr = strArr.map((cur, i) => {
+    return eval(cur);
+  })
+  return eval(strArr.join('-'));
+}
+
+// Model ==> 겁나 난해 
+
+function findMinByBracket(str) {
+  var cnt = 0;
+  for (let i = 1; i < str.length ; i +=1 ) {
+    if (str[i] == "+") {
+      cnt += 1;
+    } else if (str[i] == '-') {
+      break;
+    }
+  }
+  var separators = [' ', '+', '-'];
+  var tokens = str.split(new RegExp('[' + separators.join('') + ']', 'g'));
+
+  var sum = Number(tokens[0]);
+  console.log(tokens, cnt);
+  var temp = 0;
+  for(let i = 1; i < tokens.length; i += 1) {
+    if (temp < cnt) {
+      sum += Number(tokens[i]);
+    } else {
+    sum -= Number(tokens[i]);
+    }
+    temp ++;
+  }
+  return sum;
+}
+
+/*
+문자열이 주어졌을때, numberSearch(str) 함수는 주어진 문자열에서 숫자를 모두 찾아 더한 뒤 해당 값을 
+(숫자, 공백 등을 제외한) 알파벳의 길이의 길이로 나눈 값을 정수로 반올림하여 반환합니다.
+예시: 만약 문자열 Hello6 9World 2, Nic8e D7ay! 이 주어졌다면, 결과값은 2 입니다.
+우선 모든 숫자를 더하면 6 + 9 + 2 + 8 + 7 = 32 가 나오게됩니다.
+그 후 (숫자, 공백 등을 제외한) 알파벳의 길이가 17이기 때문에 32/17 = 1.882 가 나오게 되며, 
+가까운 정수로 반올림을 하게되면 정답인 2가 나오게 됩니다.
+*/
+
+// My 
+
+function numberSearch(str) {
+  // 번호 추출 후 더하기
+  // 숫자, 공백 뺀 알파벳 길이 구하기
+  // 숫자를 알파벳의 길이로 나누고, 반올림해서 리턴!!
+
+  let strArr = str.split('');
+  let numSum = str.match(/\d/g).reduce((a,c) => parseInt(a) + parseInt(c));
+
+  strArr = strArr.filter(cur => cur.match(/\w/g) && !(cur.match(/\d+/g)));
+  let strLeng = strArr.length;
+
+  return Math.round(numSum / strLeng);
+}
+
+// Model 
+// 대박 string 으로 '1' > '0' 이나 'h' > 'a' 될줄 몰랐음 
+// 대박!!!!!!!
+function numberSearch(str) {
+  let numSum = 0;
+  let strCnt = 0;
+  for (let i = 0 ; i < str.length; i += 1) {
+    if (str[i] >= '0' && str[i] <= '9') {
+      numSum = numSum + Number(str[i]);
+    } else if((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z')) {
+      strCnt = strCnt + 1;
+    }
+  }
+  return Math.round(numSum/strCnt);
+}
 
 
 
