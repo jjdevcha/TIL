41e168e541e827b8c6d179746504978916f18d31
Jungincha <jungincha923@gmail.com>
2019-10-25 15:56:27 +0900

    20191025 Algorithms

--- Algorithm/solve.js
+++ Algorithm/solve.js
@@ -1337,3 +1337,132 @@ function truthCheck(collection, pre) {
   // Is everyone being true?
   return collection.every(obj => obj[pre]);
 }
+
+/*
+Create a function that sums two arguments together. If only one argument is provided, 
+then return a function that expects one argument and returns the sum.
+For example, addTogether(2, 3) should return 5, and addTogether(2) should return a function.
+Calling this returned function with a single argument will then return the sum:
+var sumTwoAnd = addTogether(2);
+sumTwoAnd(3) returns 5.
+*/
+
+// My 
+// 커링을 이용해서 푼 재밌는 문제
+// 헷갈렸던 건 커링으로 풀 때가 아니면 그냥 함수 하나만으로도 리턴이 가능하게 해야한다는점 
+
+function addTogether(...args) {
+  if(args.every(el => typeof el === "number")) {
+
+    if(args.length > 1) {
+      return args.reduce((a, c) => a + c);
+    }else {
+      return b => {
+          if(typeof b === "number") {
+            return args[0] + b;
+          }
+        }
+      }
+  }
+}
+
+/*
+Fill in the object constructor with the following methods below:
+getFirstName()
+getLastName()
+getFullName()
+setFirstName(first)
+setLastName(last)
+setFullName(firstAndLast)
+*/
+
+// My 
+
+var Person = function(firstAndLast) {
+  
+  let nameArr = firstAndLast.split(' ');
+  let firstName = nameArr[0];
+  let lastName = nameArr[1];
+  let returnName = [firstName, lastName];
+  
+
+  this.getFirstName = () => returnName[0];
+
+  this.getLastName = () => returnName[1];
+
+  this.getFullName = () => returnName.join(' ');
+
+  this.setFirstName = first => {returnName[0] = first};
+
+  this.setLastName = last => {returnName[1] = last};
+
+  this.setFullName = firstAndLast => {
+    returnName = firstAndLast.split(' '); 
+    
+  };
+
+};
+
+// Model 
+
+var Person = function(firstAndLast) {
+  var fullName = firstAndLast;
+
+  this.getFirstName = function() {
+    return fullName.split(" ")[0];
+  };
+
+  this.getLastName = function() {
+    return fullName.split(" ")[1];
+  };
+
+  this.getFullName = function() {
+    return fullName;
+  };
+
+  this.setFirstName = function(name) {
+    fullName = name + " " + fullName.split(" ")[1];
+  };
+
+  this.setLastName = function(name) {
+    fullName = fullName.split(" ")[0] + " " + name;
+  };
+
+  this.setFullName = function(name) {
+    fullName = name;
+  };
+};
+
+var bob = new Person("Bob Ross");
+bob.getFullName();
+
+/*
+Return a new array that transforms the elements' average altitude into their orbital periods (in seconds).
+The array will contain objects in the format {name: 'name', avgAlt: avgAlt}.
+You can read about orbital periods on Wikipedia.
+The values should be rounded to the nearest whole number. The body being orbited is Earth.
+The radius of the earth is 6367.4447 kilometers, and the GM value of earth is 398600.4418 km3s-2.
+*/
+
+// My 
+
+function orbitalPeriod(arr) {
+  var GM = 398600.4418;
+  var earthRadius = 6367.4447;
+
+  let orbitalP = alt =>  {
+   return Math.round(
+   2 * Math.PI * Math.sqrt(Math.pow(earthRadius + alt, 3) / GM)
+   );
+  }
+  
+  arr.forEach(curObj => {
+    curObj.orbitalPeriod = orbitalP(curObj.avgAlt);
+    delete curObj.avgAlt
+  });
+  
+  return arr;
+}
+
+// Model
+// similar with mine
