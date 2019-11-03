bd0060bc00fc1aa3d31436ea389283b84336265a
Jungincha <jungincha923@gmail.com>
2019-11-03 22:18:17 +0900

    20191103 Algorithm

--- Algorithm/solve.js
+++ Algorithm/solve.js
@@ -2167,4 +2167,73 @@ function ClosestEnemyII(strArr) {
 function distance(index1, index2, len) {
 	return Math.min(Math.abs(index2 - index1), len - Math.abs(index2 - index1));
 }
+
+/*
+Vowel Square
+Have the function VowelSquare(strArr) take the strArr parameter being passed which will be a 2D matrix of some arbitrary size filled with letters from the alphabet, 
+and determine if a 2x2 square composed entirely of vowels exists in the matrix. 
+For example: strArr is ["abcd", "eikr", "oufj"] then this matrix looks like the following:
+
+a b c d
+e i k r
+o u f j
+
+Within this matrix there is a 2x2 square of vowels starting in the second row and first column, namely, ei, ou. 
+If a 2x2 square of vowels is found your program should return the top-left position (row-column) of the square, 
+so for this example your program should return 1-0. 
+If no 2x2 square of vowels exists, then return the string not found. 
+If there are multiple squares of vowels, return the one that is at the most top-left position in the whole matrix. 
+The input matrix will at least be of size 2x2.
+*/
+
+// My
+
+function VowelSquare(strArr) { 
+  const vowels = ['a', 'e', 'i', 'o', 'u'];
+  
+  for (let row = 0; row < strArr.length; row++) {
+    for (let col = 0; col < strArr[0].length; col++) {
+      if(vowels.includes(strArr[row][col]) && vowels.includes(strArr[row][col + 1])) {
+        if(vowels.includes(strArr[row + 1][col]) && vowels.includes(strArr[row + 1][col + 1])) {
+          return `${row}-${col}`;
+        }
+      }
+    }
+  }
+  return "not found"; 
+
+}
+
+// Model
+// By making vowel check func and check func it looks more organized
+function VowelSquare(strArr) { 
+
+  for (let row = 0; row < strArr.length-1; row++) {
+      for (let col = 0; col < strArr[0].length-1; col++) {
+          if (isVowels2x2(strArr, row, col)) {
+              // Good!  Return first one because we
+              // searche in a good order
+              return row + '-' + col;
+          }
+      }
+  }
+  return 'not found';
+       
+       
+  function isVowels2x2(strArr, row, col) {
+      // Could do bounds checking here but it is done in main()
+      
+      if (!isVowel(strArr[row][col])) return false;
+      if (!isVowel(strArr[row+1][col])) return false;
+      if (!isVowel(strArr[row][col+1])) return false;
+      if (!isVowel(strArr[row+1][col+1])) return false;
+      return true;
+  }
+  
+  function isVowel(letter) {
+      const VOWELS = 'aeiou';
+      return VOWELS.includes(letter);
+  }
+}
+ 
    
