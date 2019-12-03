df1f6baffb753065162919a84b58cb94d6f2f5da
Jungincha <jungincha923@gmail.com>
2019-12-03 22:43:34 +0900

    20191203 Algorithm

--- Algorithm/solve.js
+++ Algorithm/solve.js
@@ -3286,3 +3286,50 @@ function validate(password) {
 // 9999, 1100 => 11099
 // 9999, 110 => 
 
+
+/**
+ * KATA
+ * Phone Directory
+ */
+
+ // My 
+ // 로그는 정상적으로 다 찍히는데 테스트가 통과를 안한다... 노드 문제이길 
+ 
+ const dr = "/+1-541-754-3010 156 Alphand_St. <J Steeve>\n 133, Green, Rd. <E Kustur> NY-56423 ;+1-541-914-3010\n"
++ "+1-541-984-3012 <P Reed> /PO Box 530; Pollocksville, NC-28573\n :+1-321-512-2222 <Paul Dive> Sequoia Alley PQ-67209\n"
++ "+1-741-984-3090 <Peter Reedgrave> _Chicago\n :+1-921-333-2222 <Anna Stevens> Haramburu_Street AA-67209\n"
++ "+1-111-544-8973 <Peter Pan> LA\n +1-921-512-2222 <Wilfrid Stevens> Wild Street AA-67209\n"
++ "<Peter Gone> LA ?+1-121-544-8974 \n <R Steell> Quora Street AB-47209 +1-481-512-2222\n"
++ "<Arthur Clarke> San Antonio $+1-121-504-8974 TT-45120\n <Ray Chandler> Teliman Pk. !+1-681-512-2222! AB-47209,\n"
++ "<Sophia Loren> +1-421-674-8974 Bern TP-46017\n <Peter O'Brien> High Street +1-908-512-2222; CC-47209\n"
++ "<Anastasia> +48-421-674-8974 Via Quirinal Roma\n <P Salinger> Main Street, +1-098-512-2222, Denver\n"
++ "<C Powel> *+19-421-674-8974 Chateau des Fosses Strasbourg F-68000\n <Bernard Deltheil> +1-498-512-2222; Mount Av.  Eldorado\n"
++ "+1-099-500-8000 <Peter Crush> Labrador Bd.\n +1-931-512-4855 <William Saurin> Bison Street CQ-23071\n"
++ "<P Salinge> Main Street, +1-098-512-2222, Denve\n"
+
+function phone(string, num) {
+    let stringArr = string.split('\n');
+    const numReg = RegExp(num);
+    let res = `Phone => ${num}`;
+    
+    let resArr = stringArr.filter(cur => cur.match(numReg));
+    if(resArr.length > 1) return `Error => Too many people: ${num}`;
+    if(resArr.length < 1) return `Error => Not found: ${num}`;
+
+    for (let i = 0; i < stringArr.length; i++) {
+      if(numReg.test(stringArr[i])) {
+        console.log(stringArr[i])
+
+        let name = /(<\w+ \w+>)|(<\w+>)/g.exec(stringArr[i])[0];
+        name = name.substring(1, name.length - 1);
+
+        const address = stringArr[i].replace(/<.+>|\+\d\d?-(\d{3}-){2}\d{4}|[;$*/?,:!]/g, "").replace(/_| +/g, " ");
+    
+        res += `, Name => ${name}, Address => ${address}`;
+        return res;
+      }
+    }
+}
+phone(dr, "48-421-674-8974")// "Phone => 48-421-674-8974, Name => Anastasia, Address => Via Quirinal Roma")
+
+
