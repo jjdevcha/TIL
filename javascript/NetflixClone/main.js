021b79ba034c1952f14231bde33782edc040bbf6
jungincha <47588349+Jungincha@users.noreply.github.com>
2022-06-23 22:59:59 +1000

    Add files via upload

---
+++ javascript/NetflixClone/main.js
@@ -0,0 +1,53 @@
+// var, let, const
+// you don't wanna use var anymore
+
+// let age = 30;
+// // const is fixed variable
+// console.log(age);
+
+// String, Numbers, Boolean, null, undefined
+
+// const name = 'John';
+// const age3 = 30;
+// // const isCool = true;
+// // const rating = 4.5;
+// // const x = null;
+// // const  y = undefined;
+// // let z;
+
+// // console.log(typeof z);
+
+// // null value returning as object is bogus, it is actually null
+
+// // Concatenation
+// // Template String
+// console.log(`My name is ${name} and I am ${age3}years old`);
+
+// Object literals
+// Object destruct
+
+// class Person {
+//     constructor(firstName, lastName, dob) {
+//         this.firstName = firstName;
+//         this.lastName = lastName;
+//         this.dob = new Date(dob);
+//     }
+
+//     getBirthYear() {
+//         return this.dob.getFullYear();
+//     }
+
+//     getFullName() {
+//         return `${this.firstName} ${this.lastname}`;
+//     }
+// }
+
+// const person1 = new Person('John', 'Doe', '4-3-1980');
+// const person2 = new Person('Mary', 'Smith', '3-6-1970');
+
+// console.log(person2.getFullName());
+// console.log(person1);
+
+// Single element
+const form = document.getElementById('my-form');
+const container = document.querySelector('.container');
