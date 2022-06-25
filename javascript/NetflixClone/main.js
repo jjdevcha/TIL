b37e16299c2815147a9878f830594a71dbe31090
jungincha <47588349+Jungincha@users.noreply.github.com>
2022-06-25 19:35:17 +1000

    Add files via upload

--- javascript/NetflixClone/main.js
+++ javascript/NetflixClone/main.js
@@ -1,53 +1,26 @@
-// var, let, const
-// you don't wanna use var anymore
+const tabItems = document.querySelectorAll('.tab-item');
+const tabContentItems = document.querySelectorAll('.tab-content-item');
 
-// let age = 30;
-// // const is fixed variable
-// console.log(age);
+// Select tab content item
+function selectItem(e) {
+    removeBorder();
+    removeShow();
+    // Add border to current tab
+    this.classList.add('tab-border');
 
-// String, Numbers, Boolean, null, undefined
+    //Grab content item from DOM
+    const tabContentItem = document.querySelector(`#${this.id}-content`)
+    tabContentItem.classList.add('show');
+}
 
-// const name = 'John';
-// const age3 = 30;
-// // const isCool = true;
-// // const rating = 4.5;
-// // const x = null;
-// // const  y = undefined;
-// // let z;
+function removeBorder() {
+    tabItems.forEach(item => item.classList.remove('tab-border'));
+}
 
-// // console.log(typeof z);
+function removeShow() {
+    tabContentItems.forEach(item => item.classList.remove('show'));
+}
 
-// // null value returning as object is bogus, it is actually null
+// Listen for tab click
+tabItems.forEach(item => item.addEventListener('click', selectItem));
 
-// // Concatenation
-// // Template String
-// console.log(`My name is ${name} and I am ${age3}years old`);
-
-// Object literals
-// Object destruct
-
-// class Person {
-//     constructor(firstName, lastName, dob) {
-//         this.firstName = firstName;
-//         this.lastName = lastName;
-//         this.dob = new Date(dob);
-//     }
-
-//     getBirthYear() {
-//         return this.dob.getFullYear();
-//     }
-
-//     getFullName() {
-//         return `${this.firstName} ${this.lastname}`;
-//     }
-// }
-
-// const person1 = new Person('John', 'Doe', '4-3-1980');
-// const person2 = new Person('Mary', 'Smith', '3-6-1970');
-
-// console.log(person2.getFullName());
-// console.log(person1);
-
-// Single element
-const form = document.getElementById('my-form');
-const container = document.querySelector('.container');
