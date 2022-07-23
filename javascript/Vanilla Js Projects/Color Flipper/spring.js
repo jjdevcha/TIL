4f3c34c3a779ffe5f6657955f5fe732540050554
jungincha <47588349+Jungincha@users.noreply.github.com>
2022-07-23 20:32:03 +1000

    Add files via upload

---
+++ javascript/Vanilla Js Projects/Color Flipper/spring.js
@@ -0,0 +1,15 @@
+const colors = ["#f6bafd", "#8cd3c4", "#ffc412", "#ffa300", "#ff7d00"];
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+
+btn.addEventListener('click', function() {
+    // get random number between 0 - 3 eg) colors[0]
+    const randomNumber = getRandomNumber();
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+});
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * colors.length);
+}
+
